65bd0d4f9d1e790d484a07ca95c49e20ebcc6ef1
Boris Yakubchik <yboris@yahoo.com>
2026-07-17 14:28:25 -0400

    get system uptime

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,25 @@
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+use system_uptime::get_os_uptime_duration;
+
+fn get_uptime() {
+    match get_os_uptime_duration() {
+        Ok(uptime) => {
+            let days = uptime.as_secs() / 86400;
+            let hours = (uptime.as_secs() % 86400) / 3600;
+            let minutes = (uptime.as_secs() % 3600) / 60;
+            let seconds = uptime.as_secs() % 60;
+
+            println!("System Uptime: {}d {}h {}m {}s", days, hours, minutes, seconds);
+        }
+        Err(e) => eprintln!("Failed to get uptime: {}", e),
+    }
+}
+
+
 #[tauri::command]
 fn greet(name: &str) -> String {
+
+    let _ = get_uptime();
+
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
