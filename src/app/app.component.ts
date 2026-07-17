70750a73ea4c823ef6825cd4be95194cf9f7555b
Boris Yakubchik <yboris@yahoo.com>
2026-07-17 14:25:13 -0400

    settings persist after closing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,7 @@ import { RouterOutlet } from "@angular/router";
 import { defaultWindowIcon } from "@tauri-apps/api/app";
 import { enable, isEnabled, disable } from '@tauri-apps/plugin-autostart';
 import { invoke } from "@tauri-apps/api/core";
+import { load } from '@tauri-apps/plugin-store';
 import { Menu } from "@tauri-apps/api/menu";
 import { TrayIcon } from '@tauri-apps/api/tray';
 
@@ -14,6 +15,9 @@ import { TrayIcon } from '@tauri-apps/api/tray';
   styleUrl: "./app.component.css",
 })
 export class AppComponent implements OnInit {
+
+  store: any;
+
   greetingMessage = "";
 
   constructor() { }
@@ -44,6 +48,7 @@ export class AppComponent implements OnInit {
     console.log(tray);
 
     // this.enableAutostart();
+    this.handleSettings();
   }
 
   async enableAutostart() {
@@ -53,6 +58,19 @@ export class AppComponent implements OnInit {
     console.log(`registered for autostart? ${await isEnabled()}`);
     // Disable autostart
     disable();
+  }
+
+  async handleSettings() {
+    const defaults = {
+      'hi': 'hello world',
+      'hihi': 'auto saved to store'
+    };
+    this.store = await load('store.json', { autoSave: true, defaults });
+    const savedTheme = await this.store.get('theme');
+    const hi = await this.store.get('hi');
+    console.log('STORE:');
+    console.log(savedTheme);
+    console.log(hi);
   }
 
   greet(event: SubmitEvent, name: string): void {
